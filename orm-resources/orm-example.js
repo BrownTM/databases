8daab8c990fbaa20b63841f27e5137ef3103a053
Alastair Williams <al@Alastairs-iMac.hsd1.co.comcast.net>
2019-06-27 21:49:12 -0600

    day2 notgress

--- orm-resources/orm-example.js
+++ orm-resources/orm-example.js
@@ -4,28 +4,84 @@
  */
 
 var Sequelize = require('sequelize');
-var db = new Sequelize('chatter', 'root', '');
+var db = new Sequelize('chat', 'root', 'passkey');
 /* TODO this constructor takes the database name, username, then password.
  * Modify the arguments if you need to */
 
 /* first define the data structure by giving property names and datatypes
  * See http://sequelizejs.com for other datatypes you can use besides STRING. */
 var User = db.define('User', {
+  // id: {
+  //   type: Sequelize.INTEGER,
+  //   primaryKey: true
+  // },
   username: Sequelize.STRING
+}, {
+  timestamps: false
+});
+
+var Room = db.define('Room', {
+  // id: {
+  //   type: Sequelize.INTEGER,
+  //   primaryKey: true
+  // },
+  roomname: Sequelize.STRING
+}, {
+  timestamps: false
 });
 
 var Message = db.define('Message', {
-  userid: Sequelize.INTEGER,
-  text: Sequelize.STRING,
-  roomname: Sequelize.STRING
+  'user_id': {
+    type: Sequelize.INTEGER,
+    references: {
+      model: 'users',
+      key: 'id'
+    } 
+  },
+  'text': Sequelize.STRING,
+  'room_id': {
+    type: Sequelize.INTEGER,
+    references: {
+      model: 'rooms',
+      key: 'id'
+    } 
+  }
 });
+
+//Message.hasMany(User);
+//Message.hasMany(Room);
+
+/*Message.create({'username': 'Jean Valjean', text: 'Working?', 'roomname': 'main'}).then(
+  (msg) => { console.log(JSON.stringify(msg)); }
+);*/
 
 /* Sequelize comes with built in support for promises
  * making it easy to chain asynchronous operations together */
-User.sync()
+/*User.create({username: 'Jimbo'}, {ignore: true})
+  .then(User.find({
+    where: {
+      username: 'Baconator'
+    }
+  }).then((user) => { console.log(JSON.stringify(user)); }));*/
+
+let foreignKeys = {};
+User.find({
+  where: {
+    username: 'Baconator'
+  }
+}).then((user) => {
+  if (!user) {
+    return User.create({username: 'Baconator'}, {ignore: true});
+  } else {
+    return user;
+  }
+}).then((user) => {
+  foreignKeys['user_id'] = user.id;
+});
+/*User.sync()
   .then(function() {
     // Now instantiate an object and save it:
-    return User.create({username: 'Jean Valjean'});
+    return 
   })
   .then(function() {
     // Retrieve objects from the database:
@@ -42,3 +98,24 @@ User.sync()
     console.error(err);
     db.close();
   });
+
+Room.sync()
+  .then(function() {
+    // Now instantiate an object and save it:
+    return User.create({roomname: 'lobby'});
+  })
+  .then(function() {
+    // Retrieve objects from the database:
+    return User.findAll({ where: {roomname: 'lobby'} });
+  })
+  .then(function(users) {
+    users.forEach(function(user) {
+      console.log(room.roomname + ' exists');
+    });
+    db.close();
+  })
+  .catch(function(err) {
+    // Handle any error in the chain
+    console.error(err);
+    db.close();
+  });*/
